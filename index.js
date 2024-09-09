11ba965cf9732234e046a9c528e652065a6c7081
unknown <datdd>
2024-09-10 01:16:02 +0700

    update UI

--- index.js
+++ index.js
@@ -143,3 +143,20 @@ function copyPhoneCd() {
    // Copy the text inside the text field
   navigator.clipboard.writeText(copyText);
 }
+
+// start mung cuoi
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+// end mung cuoi
